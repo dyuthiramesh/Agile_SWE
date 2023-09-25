af924896bf28a2ee63eaecf67c416b39e1444615
dyuthiramesh <dyuthi.ramesh@gmail.com>
2023-09-25 12:26:18 +0530

    first commit

---
+++ gfg.py
@@ -0,0 +1,13 @@
+"""
+This is a simple script that adds two numbers and prints the result.
+"""
+
+# Use meaningful variable names
+NUMBER1 = 1
+NUMBER2 = 2
+
+# Perform the addition
+RESULT = NUMBER1 + NUMBER2
+
+# Print the result
+print(RESULT)
